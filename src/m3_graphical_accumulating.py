6a130e161eb7bf96ea8b53660d703f59d3a58d2c
bendzslt <bendzslt@rose-hulman.edu>
2018-09-10 00:00:57 -0400

    Made TODOs Done as they were done

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Lucus Bendzsa.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -109,6 +109,13 @@ def draw_parallel_lines(n, point, length, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ####################################################################
     # ------------------------------------------------------------------
+n = int
+rg.Point
+
+
+
+
+
 
 
 def run_test_draw_lines():
